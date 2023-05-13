855ee14b0b7a3a524075a61151abcdb5e8bfe8bf
levure-s <levure.sauvage@gmail.com>
2023-05-13 12:47:54 +0900

    add_sentry

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:venue_sercher/sech_venue_page.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  await SentryFlutter.init(
+    (option) {
+      option.dsn =
+          'https://ae5b62f603b14aaabc233576b61318b8@o4505174331228160.ingest.sentry.io/4505174341320704';
+    },
+    appRunner: () => runApp(const MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '会場検索アプリ',
+      title: 'Joyato',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import geolocator_apple
+import package_info_plus
+import sentry_flutter
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
+  SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
